0337aab614c4365dc59f663dcf772673aeb3da05
Hillyz <amund2003@gmail.com>
2020-12-07 18:55:18 +0100

    player movement + pipes

--- Flappyglobals.py
+++ Flappyglobals.py
@@ -3,10 +3,12 @@ import pygame as pg
 pg.init()
 clock = pg.time.Clock()
 FPS = 60
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 600
+HEIGHT = 800
+deadzone = 700
 SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("FLAPPY BIRD (REMASTERED(2020 EDITION))")
 
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
--- main.py
+++ main.py
@@ -9,27 +9,49 @@ class Player(pg.sprite.Sprite):
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH//2, HEIGHT//2)
+        self.velocity = 0.5
+        self.pos = (WIDTH//2)
 
     def gravity(self):
-        self.rect.y +=1
+        if self.velocity < 0.5:
+            self.velocity += 0.05
+        if self.rect.y <= deadzone - 20 and self.pos <= deadzone-20:
+            self.pos += self.velocity
+            self.rect.y = int(self.pos)
+        else:
+            self.rect.y = deadzone - 20
+            self.pos = deadzone - 20
 
+        if self.rect.y < 20 and self.pos < 20:
+            self.rect.y = 20
+            self.pos = 20
 
     def jump(self):
-        self.rect.y -= 60
-
-
+        self.velocity = -4
 
 
 #Pipe sprite
 class Pipe(pg.sprite.Sprite):
-    pass
+    def __init__(self, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((40, 100))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.y = y
+        self.rect.topleft = (WIDTH, self.y)
+
+    def move(self):
+        self.rect.x += 10
 
 def main():
     #local variables
     frame_counter = 0
     player = Player()
+    pipes = pg.sprite.Group()
     all_sprites = pg.sprite.Group()
     all_sprites.add(player)
+    pipe = Pipe(400)
+    pipes.add(pipe)
     # Game loop
     while True:
         #Run game at set speed
@@ -42,20 +64,26 @@ def main():
                 run = False
                 pg.quit()
                 sys.exit()
-            if event.type == pg.MOUSEBUTTONUP:
+            elif event.type == pg.MOUSEBUTTONUP:
                 player.jump()
 
         #Player movement
-        if frame_counter % 2 == 0:
+        if frame_counter % 3 == 0:
             player.gravity()
 
 
+
+        for pipe in pipes:
+            pipe.move()
+
         #Update
         all_sprites.update()
+        pipes.update()
 
         #draw
         SCREEN.fill(BLACK)
         all_sprites.draw(SCREEN)
+        pipes.draw(SCREEN)
 
         #Update display
         pg.display.update()
