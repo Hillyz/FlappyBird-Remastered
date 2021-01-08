7ce2a3788b3a22fb8c803ffcdae8647220bec0c1
Hillyz <amund2003@gmail.com>
2021-01-08 22:56:54 +0100

    Bird animation, menu optimization

--- Flappyglobals.py
+++ Flappyglobals.py
@@ -13,17 +13,22 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
+ORANGE = (255,140,0)
 
 #Font stuff
 font_name = pg.font.match_font('arial')
 font = pg.font.Font(font_name, 44)
 
 #Load graphics
+player_imgs = []
 background = pg.image.load("Background.png").convert()
 background_rect = background.get_rect()
-player_img = pg.image.load("Bird1.png").convert()
+player_img1 = pg.image.load("Bird1.png").convert()
+player_img2 = pg.image.load("Bird2.png").convert()
+player_imgs.append(player_img1)
+player_imgs.append(player_img2)
 pipe_img1 = pg.image.load("Pipe_sprite.png").convert()
 pipe_img2 = pg.image.load("Pipe_sprite2.png").convert()
 play_button = pg.image.load("Play.png").convert()
-
+score = 0
 highscore = 0
--- main.py
+++ main.py
@@ -4,32 +4,34 @@ import random
 from math import *
 
 def sigma(x):
-    return 180 / (1 + e**(-x)) - 90
+    return 180 / (1 + e**(-x)) - 75
 
 
 #Player sprite
 class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.transform.scale(player_img, (40, 40))
+        self.index = 0
+        self.image = pg.transform.scale(player_imgs[self.index%2], (40, 40))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH//2, HEIGHT//3)
         self.velocity = 20
         #Positional variable to calculate float value for y-coordinate
         self.pos = (HEIGHT//4)
+        self.dead = True #bruk til main function ting
 
     def gravity(self):
         g = 7
         self.velocity += g #Acceleration
         #Limits movement to stay in screen
-        if self.rect.y <= deadzone - 20:
+        if self.rect.y <= deadzone:
             self.pos += self.velocity #Updates float value
             self.rect.y = int(self.pos) #Updates rect value as int
         else:
             #Player dies
-            gamestate = "menu"
-            main()
+            self.dead = True
+
 
         #Movement limits top of screen
         if self.rect.y < 20 and self.pos < 20:
@@ -42,7 +44,7 @@ class Player(pg.sprite.Sprite):
 
     def bird_rotation(self):
         angle = sigma(-self.velocity*0.01)
-        new_image = pg.transform.scale(player_img, (40, 40))
+        new_image = pg.transform.scale(player_imgs[self.index%2], (40, 40))
         new_image.set_colorkey(BLACK)
         self.image = pg.transform.rotate(new_image, angle)
 
@@ -84,110 +86,136 @@ class Playbutton(pg.sprite.Sprite):
 
 
 def draw_text(surface, text, x, y):
-    text_surface = font.render(text, True, WHITE)
+    text_surface = font.render(text, True, ORANGE)
     text_rect = text_surface.get_rect()
-    text_rect.topright = (x, y)
+    text_rect.center = (x, y)
     surface.blit(text_surface, text_rect)
 
 def main():
     #local variables
     frame_counter = 0
-    player = Player()
     playbutton = Playbutton()
     menusprites = pg.sprite.Group()
+    menusprites.add(playbutton)
+    pipe_diff = 280 #Constant for the room between a pair of pipes
+    score = 0
+    highscore = 0
     pipes = pg.sprite.Group()
     players = pg.sprite.Group()
-    players.add(player)
-    menusprites.add(playbutton)
+    player = Player()
     pipe1 = Pipe(200, deadzone) #Lower pipe
     pipe2 = Pipe(HEIGHT-250, 250) #Higher pipe
     pipes.add(pipe1, pipe2)
-    pipe_diff = 280 #Constant for the room between a pair of pipes
-    score = 0
-    gamestate = "menu" #Player state
+    #players.add(player)
 
-    #Menu loop
-    while gamestate == "menu":
-        #Events
-        for event in pg.event.get():
-            #Game exit
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-                 pos = pg.mouse.get_pos()
-                 if playbutton.rect.collidepoint(pos):
-                     gamestate = "play"
-
-
-        #draw
-        SCREEN.fill(BLACK)
-        SCREEN.blit(background, background_rect)
-        menusprites.draw(SCREEN)
-        players.draw(SCREEN)
-        pipes.draw(SCREEN)
-        draw_text(SCREEN, str(score), 50, 15)
-
-        #Update display
-        pg.display.update()
-
-    # Game loop
-    while gamestate == "play":
-        #Run game at set speed
-        clock.tick(FPS)
-        frame_counter += 1
-
-        #Events
-        for event in pg.event.get():
-            #Game exit
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            #Player actions
-            elif event.type == pg.MOUSEBUTTONUP:
-                player.jump()
-
-        #Player natural movement
-        if frame_counter % 3 == 0:
-            player.gravity()
-            player.bird_rotation()
-
-        #Check for collisions
-        if player.collision(pipes):
-            gamestate = "menu"
-            return main()
+    def menu():
+        nonlocal highscore
+        nonlocal score
+        while True:
+            for event in pg.event.get():
+                #Game exit
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                     pos = pg.mouse.get_pos()
+                     if playbutton.rect.collidepoint(pos):
+                         player.dead = False
+                         return play()
 
 
-        #Pipe movement
-        for pipe in pipes:
-            pipe.move()
-            if pipe.rect.x < 0:
-                pipe.remove(pipes)
+            #draw
+            SCREEN.fill(BLACK)
+            SCREEN.blit(background, background_rect)
+            players.draw(SCREEN)
+            pipes.draw(SCREEN)
+            if score >= 0:
+                draw_text(SCREEN, f"SCORE: {score}", WIDTH//2, 50)
+            else:
+                score = 0
+            draw_text(SCREEN, f"HIGHSCORE: {highscore}", WIDTH//2, 200)
+            menusprites.draw(SCREEN)
+
+            #Update display
+            pg.display.update()
+            #bruk functions i stedet for loops og gamestate
 
 
-        #Spawn pipes
-        if frame_counter % 60 == 0:
-            pipe_len = random.randint(120, 330) #Random length of pipe
-            pipe1 = Pipe(pipe_len, deadzone) #Random bot pipe
-            pipe2 = Pipe(HEIGHT-pipe_len-pipe_diff, HEIGHT-pipe_len-pipe_diff) #Random top pipe
-            pipes.add(pipe1, pipe2)
-            score += 1
+    def play():
+        nonlocal frame_counter
+        nonlocal score
+        score = -1
+        nonlocal highscore
+        pipes.empty()
+        players.empty()
+        player = Player()
+        players.add(player)
+
+        while True:
+            #Run game at set speed
+            clock.tick(FPS)
+            frame_counter += 1
+            if frame_counter % 6 == 0:
+                player.index += 1
+
+            #Events
+            for event in pg.event.get():
+                #Game exit
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+                #Player actions
+                elif event.type == pg.MOUSEBUTTONUP:
+                    player.jump()
+
+            #Player natural movement
+            if frame_counter % 3 == 0:
+                player.gravity()
+                player.bird_rotation()
+
+            #Check for collisions
+            if player.collision(pipes) or player.rect.y >= deadzone:
+                player.dead = True
+                return menu()
 
 
-        #Update
-        players.update()
-        pipes.update()
+            #Pipe movement
+            for pipe in pipes:
+                pipe.move()
+                if pipe.rect.x < 0:
+                    pipe.remove(pipes)
 
-        #draw
-        SCREEN.fill(BLACK)
-        SCREEN.blit(background, background_rect)
-        players.draw(SCREEN)
-        pipes.draw(SCREEN)
-        draw_text(SCREEN, str(score), 50, 15)
 
-        #Update display
-        pg.display.update()
+            #Spawn pipes
+            if frame_counter % 60 == 0:
+                pipe_len = random.randint(120, 330) #Random length of pipe
+                pipe1 = Pipe(pipe_len, deadzone) #Random bot pipe
+                pipe2 = Pipe(HEIGHT-pipe_len-pipe_diff, HEIGHT-pipe_len-pipe_diff) #Random top pipe
+                pipes.add(pipe1, pipe2)
+                score += 1
+                if score > highscore:
+                    highscore = score
 
+
+            #Update
+            players.update()
+            pipes.update()
+
+            #draw
+            SCREEN.fill(BLACK)
+            SCREEN.blit(background, background_rect)
+            players.draw(SCREEN)
+            pipes.draw(SCREEN)
+            if score >= 0:
+                draw_text(SCREEN, str(score), WIDTH//2, 50)
+
+            #Update display
+            pg.display.update()
+
+    if player.dead == True:
+        menu()
+    else:
+        play()
 
 
 main()
