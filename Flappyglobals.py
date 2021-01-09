ee2acb79a9550fa17e7e8dc9fb44c0f12b854609
Hillyz <amund2003@gmail.com>
2021-01-09 16:47:28 +0100

    Added more detailed comments, cleaned up structure, removed bad code

--- Flappyglobals.py
+++ Flappyglobals.py
@@ -1,7 +1,14 @@
+#Import modules
 import pygame as pg
+import sys
+import random
+from math import *
 
+#Initialize pygame
 pg.init()
 pg.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
+
+#Global variables
 clock = pg.time.Clock()
 FPS = 60
 WIDTH = 600
@@ -10,6 +17,7 @@ deadzone = 700
 SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("FLAPPY BIRD (REMASTERED(2020/2021 EDITION))")
 
+#Colours
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
@@ -37,3 +45,22 @@ highscore = 0
 #Load sounds
 woosh_sound = pg.mixer.Sound("WOosh.wav")
 score_sound = pg.mixer.Sound("pling.wav")
+#highscore_sound = pg.mixer.Sound("highscore.wav")
+#Volume settings
+woosh_sound.set_volume(0.1)
+score_sound.set_volume(0.45)
+#highscore_sound.set_volume(1)
+
+
+#Global functions
+#Function to show text on screen, really useful in general
+def draw_text(surface, text, x, y):
+    text_surface = font.render(text, True, ORANGE)
+    text_rect = text_surface.get_rect()
+    text_rect.center = (x, y)
+    surface.blit(text_surface, text_rect)
+
+#Function to calculate acceleration
+#Used for falling speed and rotation of bird
+def sigma(x):
+    return 180 / (1 + e**(-x)) - 75
--- main.py
+++ main.py
@@ -1,82 +1,84 @@
+#Import globals
 from Flappyglobals import *
-import sys
-import random
-from math import *
 
-def sigma(x):
-    return 180 / (1 + e**(-x)) - 75
 
 
 #Player sprite
 class Player(pg.sprite.Sprite):
+    #Initialize class
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.index = 0
-        self.image = pg.transform.scale(player_imgs[self.index%2], (40, 40))
-        self.image.set_colorkey(BLACK)
+        self.index = 0 #Used to change image for animation
+        self.image = pg.transform.scale(player_imgs[self.index%2], (40, 40)) #Player graphic
+        self.image.set_colorkey(BLACK) #Removes black square behind graphic on screen
+        #Sprites are defined as rects, which makes it easy to detect collision and use coordinates
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH//2, HEIGHT//3)
-        self.velocity = 20
-        #Positional variable to calculate float value for y-coordinate
-        self.pos = (HEIGHT//4)
-        self.dead = True #bruk til main function ting
+        self.rect.center = (WIDTH//2, HEIGHT//3) #Player spawn point
+        self.velocity = 20 #Player initial falling speed
+        self.dead = True #Boolean to determine if dead or not, used to determine gamestate
 
+    #Function for falling, based on downwards acceleration
     def gravity(self):
         g = 7
         self.velocity += g #Acceleration
         #Limits movement to stay in screen
         if self.rect.y <= deadzone:
-            self.pos += self.velocity #Updates float value
-            self.rect.y = int(self.pos) #Updates rect value as int
+            self.rect.y += self.velocity #Updates velocity, bird accelerates
         else:
             #Player dies
             self.dead = True
 
 
         #Movement limits top of screen
-        if self.rect.y < 20 and self.pos < 20:
+        if self.rect.y < 20:
             self.rect.y = 20
-            self.pos = 20
+
 
     #Player action
+    #Makes the bird do its classic jump by changing the velocity to go in the opposite direction
     def jump(self):
         self.velocity = -40
 
+    #Function that makes the bird rotate around itself based on velocity
     def bird_rotation(self):
-        angle = sigma(-self.velocity*0.01)
+        angle = sigma(-self.velocity*0.01) #calculates angle in degrees based on velocity
         new_image = pg.transform.scale(player_imgs[self.index%2], (40, 40))
-        new_image.set_colorkey(BLACK)
-        self.image = pg.transform.rotate(new_image, angle)
+        new_image.set_colorkey(BLACK) #Removes black square
+        self.image = pg.transform.rotate(new_image, angle)#Rotates the new image
 
 
     #Collision function
     def collision(self, group2):
-        collision = pg.sprite.spritecollide(self, group2, False)
+        collision = pg.sprite.spritecollide(self, group2, False) #Checks collision
         if collision:
             return True
 
 
 #Pipe sprite
+#Based on rect
 class Pipe(pg.sprite.Sprite):
-    def __init__(self, h, space):
+    #Initialize sprite
+    def __init__(self, len, space):
         pg.sprite.Sprite.__init__(self)
+        #Determines whether the pipe is on top or bottom of the screen
         if space == deadzone:
-            self.image = pg.transform.scale(pipe_img1, (40, h))
+            self.image = pg.transform.scale(pipe_img1, (40, len))
         else:
-            self.image = pg.transform.scale(pipe_img2, (40, h))
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        self.h = h
-        #Positional x value for float
-        self.pos = WIDTH
+            self.image = pg.transform.scale(pipe_img2, (40, len))
 
-        self.velocity = 5
+        self.image.set_colorkey(BLACK) #Removes black square
+        self.rect = self.image.get_rect()
+        self.len = len #Undefined length variable for random height for the pipes
+        self.velocity = 5 #Speed
         self.rect.bottomleft = (WIDTH, space) #Space --> top or bot of screen
 
+    #Function to move sprite from right to left
     def move(self):
-        self.pos -= self.velocity
-        self.rect.x = self.pos
+        self.rect.x -= self.velocity
 
+
+#Sprite for the play button that shows up in the menu
+#Could perhaps simply be an image, but I thought of this first so here it is
 class Playbutton(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
@@ -85,76 +87,99 @@ class Playbutton(pg.sprite.Sprite):
         self.rect.center = (WIDTH//2, HEIGHT//2)
 
 
-def draw_text(surface, text, x, y):
-    text_surface = font.render(text, True, ORANGE)
-    text_rect = text_surface.get_rect()
-    text_rect.center = (x, y)
-    surface.blit(text_surface, text_rect)
-
+#Main function where everything is executed
 def main():
-    #local variables
-    frame_counter = 0
-    playbutton = Playbutton()
-    menusprites = pg.sprite.Group()
+
+    #Initialize sprites and add to group
+    playbutton = Playbutton() #Initializes object
+    menusprites = pg.sprite.Group() #Group for sprites in the menu
     menusprites.add(playbutton)
-    pipe_diff = 280 #Constant for the room between a pair of pipes
-    score = 0
-    highscore = 0
     pipes = pg.sprite.Group()
     players = pg.sprite.Group()
     player = Player()
     pipe1 = Pipe(200, deadzone) #Lower pipe
     pipe2 = Pipe(HEIGHT-250, 250) #Higher pipe
     pipes.add(pipe1, pipe2)
-    #players.add(player)
 
+    #Local variables
+    frame_counter = 0
+    pipe_diff = 280 #Constant for the room between a pair of pipes
+    score = 0
+    init_highscore = 0
+    highscore = init_highscore
+
+    #Nested function for menu screen
     def menu():
+        #Call nonlocal variables
         nonlocal highscore
+        nonlocal init_highscore
         nonlocal score
+
+        #Menu loop
         while True:
+            #Event loop
             for event in pg.event.get():
                 #Game exit
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+
+                #Check if mouse is clicked
                 if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                      pos = pg.mouse.get_pos()
+
+                     #Check if mouse is on the button when clicked
                      if playbutton.rect.collidepoint(pos):
                          player.dead = False
+                         #Ends menu gamestate, playing starts
                          return play()
 
+            #Checks if there is a new high score
+            if highscore > init_highscore:
+                init_highscore = highscore
+                #highscore_sound.play()
 
-            #draw
-            SCREEN.fill(BLACK)
+            #Draws everything on screen
+            SCREEN.fill(BLACK) #Clear screen
             SCREEN.blit(background, background_rect)
             players.draw(SCREEN)
             pipes.draw(SCREEN)
+
+            #Draws score on screen
+            #If statements to get around bug, quick fix
             if score >= 0:
                 draw_text(SCREEN, f"SCORE: {score}", WIDTH//2, 50)
             else:
                 score = 0
+
             draw_text(SCREEN, f"HIGHSCORE: {highscore}", WIDTH//2, 200)
             menusprites.draw(SCREEN)
 
             #Update display
             pg.display.update()
-            #bruk functions i stedet for loops og gamestate
 
-
+    #Nested function for play gamestate
     def play():
+        #variables
         nonlocal frame_counter
         nonlocal score
-        score = -1
         nonlocal highscore
+        score = -1 #Begins at -1 instead of 0 to make the algorithm to count score easier
+
+        #Clear sprite groups to reset screen
         pipes.empty()
         players.empty()
+        #Initialize player object and add to sprite group
         player = Player()
         players.add(player)
 
+        #Game loop
         while True:
             #Run game at set speed
             clock.tick(FPS)
             frame_counter += 1
+
+            #Every 6 frames, animation updates to simulate wing flapping
             if frame_counter % 6 == 0:
                 player.index += 1
 
@@ -164,6 +189,7 @@ def main():
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+
                 #Player actions
                 elif event.type == pg.MOUSEBUTTONUP:
                     player.jump()
@@ -177,6 +203,7 @@ def main():
             #Check for collisions
             if player.collision(pipes) or player.rect.y >= deadzone:
                 player.dead = True
+                #Changes gamestate from playing to menu
                 return menu()
 
 
@@ -193,9 +220,15 @@ def main():
                 pipe1 = Pipe(pipe_len, deadzone) #Random bot pipe
                 pipe2 = Pipe(HEIGHT-pipe_len-pipe_diff, HEIGHT-pipe_len-pipe_diff) #Random top pipe
                 pipes.add(pipe1, pipe2)
+
+                #Timing of passing a pipe and spawning a new one is equal
+                #Therefore the score increases simply every time a pipe is spawned
+                #However because of this, score has to start at -1 because of the first pipe that spawns
                 score += 1
+                #If to quick fix score starting at -1
                 if score > 0:
                     score_sound.play()
+                #Checks if a current score is the highest so far
                 if score > highscore:
                     highscore = score
 
@@ -209,16 +242,15 @@ def main():
             SCREEN.blit(background, background_rect)
             players.draw(SCREEN)
             pipes.draw(SCREEN)
+            #If to quick fix score starting at -1
             if score >= 0:
                 draw_text(SCREEN, str(score), WIDTH//2, 50)
 
             #Update display
             pg.display.update()
 
-    if player.dead == True:
-        menu()
-    else:
-        play()
+    #Call menu to start the program from the menu
+    menu()
 
-
+#Run the thing
 main()
